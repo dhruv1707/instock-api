8f73e5f5c210a7934733c69c3f2f14a011ab2ac1
Diego <diegopinlac@gmail.com>
2025-03-26 21:26:31 -0700

    Removed the Creation and Retrieving code and transfered it to the Routes

--- routes/warehouses-routes.js
+++ routes/warehouses-routes.js
@@ -1,20 +1,16 @@
-import initKnex from "knex";
-import configuration from "../models/knexfile.js";
-import express from "express";
+import express from 'express';
+import bodyParser from 'body-parser';
+import * as warehouseController from '../controllers/warehouses-controller.js';
 
-const knex = initKnex(configuration);
 const router = express.Router();
+router.use(bodyParser.json());
 
-router.get("/:id", async (req, res) => {
-    try {
-        const limit = 1;
-        const data = await knex("warehouses")
-        .where({ id: req.params.id })
-        .limit(parseInt(limit));
-        res.status(200).json(data);
-    } catch (err) {
-        res.status(400).send(`Error retrieving warehouses: ${err}`);
-    }
-});
+router
+  .route("/")
+  .post(warehouseController.add);
+
+router
+    .route("/:id")
+    .get(warehouseController.findOne)
 
 export default router;
