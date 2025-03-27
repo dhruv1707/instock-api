2e0c0939ab5506175dab8091c25f7cfc71a14681
Diego <diegopinlac@gmail.com>
2025-03-26 20:22:03 -0700

    Retrieved config from .env file

--- models/knexfile.js
+++ models/knexfile.js
@@ -7,11 +7,10 @@ console.log('DB password', process.env.DB_PASSWORD);
 export default {
   client: "mysql2",
   connection: {
-    host: "127.0.0.1", 
-    user: "instock",
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "instock_DB",
-    charset: "utf8"
   },
   migrations: {
     directory: './migrations'
