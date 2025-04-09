f2af92177d45e8cd9f2c50441db363a49c53c8e0
Yengzzkie <gatchalian.manuel@gmail.com>
2025-04-09 19:20:34 -0400

    fixed indentations

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -4,19 +4,19 @@ const knex = initKnex(configuration);
 
 // get the inventory for a specific warehouse based on the warehouse_id from req.params.id
 const getInventoryByWarehouseId = async (req, res) => {
-    try {
-        const response = await knex("inventories").where({ warehouse_id: req.params.id});
+  try {
+    const response = await knex("inventories").where({
+      warehouse_id: req.params.id,
+    });
 
-        if (!response) {
-            res.status(404).json({ message: `Warehouse with ID ${req.params.id} not found` })
-        }
-
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(500).json({
-            message: `Unable to retrieve inventory data for warehouse with ID ${req.params.id}`,
-          });
+    if (!response) {
+      res.status(404).json({ message: `Warehouse with ID ${req.params.id} not found` });
     }
-}
 
-export { getInventoryByWarehouseId }
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json({ message: `Unable to retrieve inventory data for warehouse with ID ${req.params.id}`});
+  }
+};
+
+export { getInventoryByWarehouseId };
